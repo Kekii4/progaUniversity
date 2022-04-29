6f5daf1966635279ba23d8d1d41e93e9e22a3d3f
Artem <ffffgfgfgfgf2@gmail.com>
2022-04-29 15:38:34 +0500

    fixed margins

--- task5.c
+++ task5.c
@@ -28,63 +28,59 @@ int main(){
         for (int j = 0; j < SIZE_ROW; j++){
             
             boolFlag = 0;
-            
-            if (arr[i][j]){
-                if (arr[i-1][j] != 1 && arr[i-1][j-1] != 1 && arr[i][j-1] != 1){
 
-                    Width = i + 1;
-                    Length = j + 1;
+            if (arr[i][j] && arr[i-1][j] != 1 && arr[i-1][j-1] != 1 && arr[i][j-1] != 1){
 
-                    while (arr[Width][j] == 1){
-                        if (arr[Width][j-1] == 1){
+                Width = i + 1;
+                Length = j + 1;
+                while (arr[Width][j] == 1){
+                    if (arr[Width][j-1] == 1){
+                        boolFlag = 1;
+                        break;
+                    }
+                    Width++;
+                }
+                if (boolFlag){
+                    continue;
+                }                   
+                
+                while (arr[i][Length] == 1){
+                    if (arr[i-1][Length] == 1){
+                        boolFlag = 1;
+                        break;
+                    }
+                    Length++;
+                }
+                if (boolFlag){
+                    continue;
+                }
+                if (arr[Width][Length] == 1){
+                    continue;
+                }
+
+                for (int byWidth = i + 1; byWidth < Width; byWidth++){
+                    if (arr[byWidth][Length] == 1){
+                        boolFlag = 1;
+                        break;
+                    }
+
+                    for (int byLength = j + 1; byLength < Length; byLength++){
+                        if (arr[byWidth][byLength] != 1){
                             boolFlag = 1;
                             break;
                         }
-                        Width++;
+                        if (byWidth == i + 1 && arr[Width][byLength] == 1){
+                            boolFlag = 1;
+                            break;
+                        }    
                     }
                     if (boolFlag){
-                        continue;
-                    }                   
-                    
-                    while (arr[i][Length] == 1){
-                        if (arr[i-1][Length] == 1){
-                            boolFlag = 1;
-                            break;
-                        }
-                        Length++;
+                        break;
                     }
-                    if (boolFlag){
-                        continue;
-                    }
+                }
 
-                    if (arr[Width][Length] == 1){
-                        continue;
-                    }
-
-                    for (int byWidth = i + 1; byWidth < Width; byWidth++){
-                        if (arr[byWidth][Length] == 1){
-                            boolFlag = 1;
-                            break;
-                        }
-
-                        for (int byLength = j + 1; byLength < Length; byLength++){
-                            if (arr[byWidth][byLength] != 1){
-                                boolFlag = 1;
-                                break;
-                            }
-                            if (byWidth == i + 1 && arr[Width][byLength] == 1){
-                                boolFlag = 1;
-                                break;
-                            }    
-                        }
-                        if (boolFlag){
-                            break;
-                        }
-                    }
-
-                    if (!boolFlag){
-                        numberRectangles++;
-                    }
+                if (!boolFlag){
+                    numberRectangles++;
                 }
             }
         }
